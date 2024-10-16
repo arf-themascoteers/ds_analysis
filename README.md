20224e987225fd128025f973f2f1579ea54c75f7
Arif <arif@themascoteers.com>
2024-10-17 08:45:50 +1100

    done

--- README.md
+++ README.md
@@ -4,10 +4,4 @@ GHISACONUS: https://e4ftl01.cr.usgs.gov/COMMUNITY/GHISACONUS.001/2008.01.01/GHIS
 LUCAS: https://esdac.jrc.ec.europa.eu/content/lucas-2009-topsoil-data
 
 
-Indian Pines: https://doi.org/10.4231/R7RX991C
-
-
-Salinas: https://doi.org/10.4231/R7RX991C
-
-
-Pavia University: https://doi.org/10.4231/R7RX991C
+Hyp: https://www.ehu.eus/ccwintco/index.php/Hyperspectral_Remote_Sensing_Scenes
---
+++ hyp.py
@@ -0,0 +1,26 @@
+import numpy as np
+import scipy.io as sio
+import pandas as pd
+
+
+def dump(X_file,gt_file,dump_file,X_key,gt_key):
+    X_img = sio.loadmat(X_file)
+    gt_img = sio.loadmat(gt_file)
+    X = X_img.get(X_key).astype('float64')
+    gt = gt_img.get(gt_key).astype('float64')
+
+    X = X.reshape(X.shape[0] * X.shape[1], X.shape[2])
+    gt = gt.reshape(-1)
+
+    columns = [str(i) for i in range(X.shape[1])] + ["class"]
+    data = np.concatenate((X,gt.reshape(-1,1)), axis=1)
+    df = pd.DataFrame(columns=columns, data=data)
+    df.to_csv(dump_file, index=False)
+
+
+original_folder = "data/original/hyp"
+output_folder = "data/output/hyp"
+
+dump(f"{original_folder}/Indian_pines_corrected.mat", f"{original_folder}/Indian_pines_gt.mat", f"{output_folder}indian_pines.csv",'indian_pines_corrected','indian_pines_gt')
+dump(f"{original_folder}/PaviaU.mat", f"{original_folder}/PaviaU_gt.mat", f"{output_folder}paviaU.csv",'paviaU','paviaU_gt')
+dump(f"{original_folder}/Salinas_corrected.mat", f"{original_folder}/Salinas_gt.mat", f"{output_folder}salinas.csv",'salinas_corrected','salinas_gt')
