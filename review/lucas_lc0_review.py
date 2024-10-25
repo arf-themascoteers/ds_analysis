f85427c78c6011ddea309282c772fe3f1633d1e0
Arif <arif@themascoteers.com>
2024-10-25 14:41:01 +1100

    :wq Revert "done"
    This reverts commit 9a65a4ac6bf75fd862939df29cab6d861aeb3df3.

--- review/lucas_lc0_review.py
+++ review/lucas_lc0_review.py
@@ -2,9 +2,11 @@ import pandas as pd
 
 
 df = pd.read_csv("../data/output/lucas/lucas_lc0_r.csv")
+print(df.columns[-1])
 unique_values_with_counts = df['lc'].value_counts()
 print(unique_values_with_counts)
 
 df = pd.read_csv("../data/output/lucas/lucas_lc0_s_r.csv")
+print(df.columns[-1])
 unique_values_with_counts = df['lc'].value_counts()
 print(unique_values_with_counts)
