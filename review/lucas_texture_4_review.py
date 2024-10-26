66a830bb45358fe5c9f4a05c37ce239e65f12c4f
Arif <arif@themascoteers.com>
2024-10-26 21:03:44 +1100

    done

---
+++ review/lucas_texture_4_review.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+
+df = pd.read_csv("../data/output/lucas/lucas_texture_4_r.csv")
+unique_values_with_counts = df['texture'].value_counts()
+print(unique_values_with_counts)
+
+nan_count = df.isna().sum().sum()
+print(nan_count)
+
+rows_with_nan = df[df.isna().any(axis=1)]
+print(rows_with_nan)
+
+print(len(df))
